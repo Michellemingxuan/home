870a2b08a06b1c36207173bf48ef822a18761287
Michelle Liu <51328342+Michellemingxuan@users.noreply.github.com>
2022-06-22 23:35:20 +0800

    Update package

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/Michellemingxuan/Michellemingxuan.github.io.git";
+const repoURL = "https://github.com/Michellemingxuan/Michellemingxuan.github.io";
 
 ghpages.publish(
   pathname,
