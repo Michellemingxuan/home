71fd672f94ad3e5a9f3d6084b2a5a88f469b8576
Michelle Liu <51328342+Michellemingxuan@users.noreply.github.com>
2022-06-22 23:38:04 +0800

    Updates

---
+++ precache-manifest.8a9cec7cd4dfbf8cd9d0d3a827cf9736.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2fc57fb699ba4603d4b9b9454fcf842",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "6f33ba6bad5d7b439dcd",
+    "url": "/home/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "ce25c2174fbc1f89bcaa",
+    "url": "/home/static/js/2.689c31cf.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.689c31cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f33ba6bad5d7b439dcd",
+    "url": "/home/static/js/main.975e9fa7.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "d56a0172bdbdda6f76d169a9e116f653",
+    "url": "/home/static/media/mingxuanliu.d56a0172.jpg"
+  },
+  {
+    "revision": "72d0432a0806c5203670285226cd4c14",
+    "url": "/home/static/media/research.72d0432a.jpg"
+  },
+  {
+    "revision": "1ec58e25dedeca5537004ad87e4c13e6",
+    "url": "/home/static/media/resume.1ec58e25.pdf"
+  }
+]);
